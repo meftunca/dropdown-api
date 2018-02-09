67905e80eb3c9f97146fddd1bfe1c0dc5112a7dd
muhammed burak şentürk <buraksenturk25@gmail.com>
2018-02-10 01:41:48 +0300

    Update dropdown.js

--- dropdown.js
+++ dropdown.js
@@ -1,117 +1,148 @@
 let dropdown = (trigger, parameter) => {
-      this.trigger = trigger;
-      this.parameter = parameter;
-      let menu, triggerItem = document.querySelectorAll(this.trigger),
+    this.trigger = trigger;
+    this.parameter = parameter;
+    let menu,
+        triggerItem = document.querySelectorAll (this.trigger),
         configure = (el) => {
-          let menu = el.parentNode.querySelector(this.parameter.menu),
-            classL = menu.classList,
-            eventType = this.parameter.type || "click",
-            triggerType = eventType === "click" ? "click" : eventType === "hover" ? "mouseover" : "click",
-            hidden = this.parameter.hidden ||  "true",
-            autoPosition = this.parameter.autoPosition ||  "true",
-            NavbarCollapse = this.parameter.NavbarCollapse ||  "false",
-            showEffect = this.parameter.showEffect || "show",
-            hiddenEffect = this.parameter.hiddenEffect || "out",
-            waitingEffect = this.parameter.waitingEffect || null,
-            menuHeight = this.parameter.height || "auto",
-            menuWidth = this.parameter.width ||  "auto";
+            let menu = el.parentNode.querySelector (this.parameter.menu),
+                classL = menu.classList,
+                eventType = this.parameter.type || "click",
+                triggerType = eventType === "click"
+                    ? "click"
+                    : eventType === "hover"
+                                  ? "mouseover"
+                                  : "click",
+                hidden = this.parameter.hidden || "true",
+                autoPosition = this.parameter.autoPosition || "true",
+                NavbarCollapse = this.parameter.NavbarCollapse || "false",
+                showEffect = this.parameter.showEffect || "show",
+                hiddenEffect = this.parameter.hiddenEffect || "out",
+                waitingEffect = this.parameter.waitingEffect || null,
+                menuHeight = this.parameter.height || "auto",
+                menuWidth = this.parameter.width || "auto",
+                rect = el.getClientRects (),
+                wWidth = window.innerWidth,
+                wHeight = window.innerHeight,
+                display_hidden_position = (type) => {
+                    return menu.setAttribute ("data-dropdown-display-coord", type);
+                }, display_show_position = () => {
+                    if (menu.hasAttribute ("data-dropdown-display-coord")){
+                        let backAttr = menu.getAttribute ("data-dropdown-display-coord");
+                        menu.classList.add(backAttr);
+                        menu.removeAttribute ("data-dropdown-display-coord")
+                    }
 
-          el.addEventListener(triggerType, function() {
-            let bodyRect = document.body.getBoundingClientRect(),
-              elRect = this.getBoundingClientRect(),
-              topQuery = () => {
-                if (autoPosition === "true") {
-                  let topQuery = elRect.top - menuHeight;
-                  if (topQuery <= 0) {
-                    if (classL.contains("dropup")) {
-                      classL.remove("dropup");
+                };
+            menu.classList.add ("show");
+            let menuRect = menu.getClientRects ();
+            menu.classList.remove ("show");
+            let coordQuery = () => {
+                    rect = el.getClientRects ()[ 0 ];
+                    menuRect = menu.getClientRects ()[ 0 ];
+
+                    if (wHeight < rect.y + menuHeight) {
+                        !classL.contains ("dropup") ? classL.add ("dropup") : null;
                     }
-                    menu.style.cssText = "position:absolute;transform:translateY(" + 0 + "px);";
-                  } else {
-                    if (!classL.contains("dropup")) {
-                      classL.add("dropup");
+                    else if (rect.top < menuHeight) {
+                        classL.contains ("dropup") ? classL.remove ("dropup") : null;
+                        display_hidden_position ("dropup");
                     }
-                    menu.style.cssText = "position:absolute;transform:translateY(-" + (
-                      0) + "px);";
-                  }
-                }
-              },
-              leftQuery = () => {
-                if (autoPosition === "true") {
-                  console.log(leftQuery, elRect.right)
-                  if (elRect.right <= menuWidth) {
-                    if (!classL.contains("dropright")) {
-                      classL.remove("dropright");
+                    else if (wWidth < rect.left + menuWidth) {
+                        classL.contains ("dropright") ? classL.remove ("dropright") : classL.add ("dropleft");
+                        !classL.contains ("dropleft") ? classL.add ("dropleft") : null;
+                        display_hidden_position ("dropright");
                     }
-                    menu.style.cssText = "position:absolute;transform:translatex(" + 0 + "px);";
-                  } else if (elRect.left <= menuWidth) {
-                    if (!classL.contains("dropleft")) {
-                      classL.add("dropleft");
+                    else if (rect.left < menuWidth) {
+                        classL.contains ("dropleft") ? classL.remove ("dropleft") : classL.add ("dropright");
+                        !classL.contains ("dropright") ? classL.add ("dropright") : null;
+                        display_hidden_position ("dropleft");
                     }
-                    menu.style.cssText = "position:absolute;transform:translatex(-" + (
-                      0) + "px);";
-                  }
-                }
-              },
-              toggles = () => {
-                if (classL.contains(showEffect)) {
-                  classL.add(hiddenEffect);
-                  classL.remove(showEffect);
-                  setTimeout(function() {
-                    classL.remove(hiddenEffect);
-                  }, "400");
+                    else if (rect.left < menuWidth + rect.width && wWidth < rect.left + menuWidth + rect.width) {
+                        classL.contains ("dropleft")
+                            ? classL.remove ("dropleft")
+                            : classL.contains ("dropright")
+                            ? classL.remove ("dropright")
+                            : classL.add ("dropright");
+                    }else{
+                        display_show_position();
+                    }
+                },
+                toggles = () => {
+                    if (classL.contains (showEffect)) {
+                        classL.add (hiddenEffect);
+                        classL.remove (showEffect);
+                        setTimeout (() => {
+                            classL.remove (hiddenEffect);
+                        }, "400");
+                    } else {
+                        classL.add (showEffect);
+                        menuHeight = menu.clientHeight;
+                        menuWidth = menu.clientWidth;
+                    }
+                },
+                defaults = () => {
+                    toggles ();
+                    if (autoPosition === "true") {
+                        coordQuery ();
+                    }
+                    let clear = () => {
+                        if (classL.contains (showEffect)) {
+                            classL.add (hiddenEffect);
+                            classL.remove (showEffect);
+                            setTimeout (() => {
+                                classL.remove (hiddenEffect);
+                            }, "380");
+                        }
+                    };
+                    if (hidden === "true") {
+                        window.addEventListener ("change", clear);
+                        document.addEventListener ("click", (event) => {
+                            let isClickInside = el.contains (event.target) || menu.contains (event.target);
+                            if (!isClickInside) {
+                                clear ();
+                            }
+                        }, false);
+                    }
+
+                };
+            if (el.getAttribute ("data-collapse") === "dropdown") {
+                if (window.innerWidth <= 769) {
+                    slide (menu).toggle (100);
                 } else {
-                  classL.add(showEffect);
-                  menuHeight = menu.clientHeight;
-                  menuWidth = menu.clientWidth;
+                    defaults ();
                 }
-              },
-              defaults = () => {
-                toggles();
-                topQuery();
-                leftQuery();
-                let clear = () => {
-                  if (classL.contains(showEffect)) {
-                    classL.add(hiddenEffect);
-                    classL.remove(showEffect);
-                    setTimeout(function() {
-                      classL.remove(hiddenEffect);
-                    }, "380");
-                  }
-                }
-                if (hidden === "true") {
-                  window.addEventListener("change", clear);
-                  document.addEventListener("click", function(event) {
-                    var isClickInside = el.contains(event.target) || menu.contains(event.target);
-                    if (!isClickInside) {
-                      clear();
+                window.addEventListener ("resize", () => {
+                    if (window.innerWidth >= 769) {
+                        menu.removeAttribute ("style");
                     }
-                  }, false);
-                }
-
-              };
-            if (el.getAttribute("data-collapse") === "dropdown") {
-              if (window.innerWidth <= 769) {
-                slide(menu).toggle(100);
-              } else {
-                defaults();
-              }
-              window.addEventListener("resize", function() {
-                if (window.innerWidth >= 769) {
-                  menu.removeAttribute("style");
-                  defaults();
-                }
-              });
+                });
             } else {
-              defaults();
+                // defaults();
             }
-
-          });
+            document.addEventListener ("load", coordQuery, true);
+            window.addEventListener ("scroll", () => {
+                if (this.parameter.autoPosition === "true" && menu.classList.contains ("show")) {
+                    coordQuery ();
+                }
+            });
+            el.addEventListener (triggerType, () => {
+                defaults ();
+            });
         },
         run = (trigger) => {
-          Array.prototype.forEach.call(triggerItem, function(el) {
-            configure(el);
-          });
+
+            Array.prototype.forEach.call (triggerItem, (el) => {
+                configure (el);
+            });
         };
-      return run(triggerItem);
-    };
+    return run (triggerItem);
+};
+dropdown (".dropdown-toggle", {
+    type: 'hover ',
+    menu: '.dropdown-menu',
+    hidden: 'true',
+    autoPosition: 'true',
+    NavbarCollapse: 'true',
+    showEffect: "show",
+    hiddenEffect: "out"
+});
